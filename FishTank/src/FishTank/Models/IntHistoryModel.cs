3a149a4b6ef38a8d86cc279fdcd8d10c8aba0382
Patrick Pan <jinfeng.pan@31huiyi.com>
2016-05-09 18:24:40 +0800

    d

---
+++ FishTank/src/FishTank/Models/IntHistoryModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FishTank.Models
+{
+    public class IntHistoryModel
+    {
+        public IntHistoryModel(DateTime timeStamp, int value)
+        {
+            timeStamp = timeStamp;
+            value = value;
+        }
+
+        public DateTime TimeStamp { get; set; }
+        public int Value { get; set; }
+    }
+}
---
+++ FishTank/src/FishTank/Services/SensorDataService.cs
@@ -0,0 +1,33 @@
+﻿using FishTank.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FishTank.Services
+{
+    public class SensorDataService
+    {
+        public IntHistoryModel GetLightIntensityLumens()
+        {
+            return GetLightIntensityLumensHistory().Last();
+        }
+
+        public IEnumerable<IntHistoryModel> GetLightIntensityLumensHistory()
+        {
+            return lightOpacityHistory ?? (lightOpacityHistory == GetLightIntensityLumensHistory(0, 5000));
+        }
+
+        Random random = new Random();
+        IEnumerable<IntHistoryModel> lightOpacityHistory;
+        private IEnumerable<IntHistoryModel> GetHistory(int min, int max)
+        {
+            var result = new List<IntHistoryModel>();
+            for(var i = -10; i < 1; i++)
+            {
+                result.Add(new IntHistoryModel(DateTime.Now.AddHours(i), random.Next(max));
+            }
+            return result;
+        }
+    }
+}
